3814b15127cf76205d00eeca33f623c28f380285
Hosein110011 <immortalhosein110011@gmail.com>
2024-10-30 22:44:10 +0330

    fix: some bugs

--- src/main/resources/static/assets/js/crypto-datatable.js
+++ src/main/resources/static/assets/js/crypto-datatable.js
@@ -1,14 +1,28 @@
 //______Data-Table
 
-$('#crypto-data-table').DataTable({
-    order: [],
-    columnDefs: [{ orderable: false, targets: '_all' }],
-    language: {
-        searchPlaceholder: 'Search...',
-        sSearch: '',
-    }
-});
+var htmlTag = document.documentElement
 
+var dir = htmlTag.dir
+
+if (dir === "rtl") {
+    $('#crypto-data-table').DataTable({
+        order: [],
+        columnDefs: [{ orderable: false, targets: '_all' }],
+        language: {
+            searchPlaceholder: 'جستجو...',
+            sSearch: '',
+        }
+    });
+} else {
+    $('#crypto-data-table').DataTable({
+        order: [],
+        columnDefs: [{orderable: false, targets: '_all'}],
+        language: {
+            searchPlaceholder: 'Search...',
+            sSearch: '',
+        }
+    });
+}
 // Select2
 
 $('.dataTables_length select').select2({
