5fd7a315e8546d05ddbc100f28dd3af9be4997a0
Sara Poursanati <sara_poursanati@outlook.com>
2024-11-04 13:27:49 +0330

    Resource moved or added.

---
+++ src/test/java/ir/niopdc/station/StationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.niopdc.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
